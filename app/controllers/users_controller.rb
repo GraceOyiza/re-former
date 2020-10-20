85e39a5c76dd849c66a90156ca4dd9f7ec9b6004
NtwaliHeritier <heritierntwali@gmail.com>
2020-10-20 12:30:17 +0200

    add update function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,24 @@ class UsersController < ApplicationController
         # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
          @user=User.new(user_params)
         if @user.save
-            redirect_to new_user_path
+            redirect_to user_path(@user)
         else    
             render :new
         end
+    end
 
+    def show
+        @user=User.find(params[:id])
+    end
+
+    def edit
+        @user=User.find(params[:id])
+    end
+
+    def update
+        @user=User.find(params[:id])
+        @user.update(user_params)
+        redirect_to user_path(@user)
     end
 
     private
